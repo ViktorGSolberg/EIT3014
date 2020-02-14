23b4cc3bad95d768255a537b047dd38aaeacc2f4
Jesper Brynildsen <jesper_bry@hotmail.com>
2020-02-14 16:42:04 +0100

    Update Park schema to type Array

--- models/Park.js
+++ models/Park.js
@@ -3,30 +3,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema,
   ObjectId = mongoose.Schema.Types.ObjectId;
 
+const arrStripsLimit = val => {
+  return val.length !== 10;
+};
+
+const arrBoxLimit = val => {
+  return val.length !== 9;
+};
+
 const parkSchema = new Schema({
   id: ObjectId,
   ledStrips: {
-    strip_1: String,
-    strip_2: String,
-    strip_3: String,
-    strip_4: String,
-    strip_5: String,
-    strip_6: String,
-    strip_7: String,
-    strip_8: String,
-    strip_9: String,
-    strip_10: String
+    type: Array,
+    validate: [arrStripsLimit, "{PATH} exceeds the limit of 10"]
   },
   ledBoxes: {
-    box_1: String,
-    box_2: String,
-    box_3: String,
-    box_4: String,
-    box_5: String,
-    box_6: String,
-    box_7: String,
-    box_8: String,
-    box_9: String
+    type: Array,
+    validate: [arrBoxLimit, "{PATH} exceeds the limit of 9"]
   },
   screen_1: String,
   screen_2: String

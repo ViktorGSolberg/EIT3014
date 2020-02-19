35642066a0747bc7699a02a59dce43934a79a0f0
Viktor <viktorgsolberg@gmail.com>
2020-02-19 11:21:58 +0100

    Lagt til react-router, lagt til materialUI, satt opp struktur for frontend.

---
+++ client/src/pages/Adminpage/Adminpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Homepage extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h1>Adminsiden :D</h1>
+    </div>
+    );
+  }
+}
+export default Homepage;

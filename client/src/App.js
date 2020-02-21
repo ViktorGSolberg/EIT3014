4f7cd4860323a4ef58098ef00be59c23bde63e6c
Viktor <viktorgsolberg@gmail.com>
2020-02-21 13:57:12 +0100

    Homepage is now set correctly

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import Adminpage from './pages/Adminpage/Adminpage';
-import Blogpage from './pages/Blogpage/Blogpage'
+
+
 
 const backgroundColor = "white";
 
 class App extends Component {
+  
   render() {
+
     const App = () => (
       <div style={{backgroundColor: backgroundColor}}>
         <Switch>
           <Route exact path='/' component={Homepage}/>
-          <Route exact path='/blog' component={Blogpage}/>
           <Route path='/admin' component={Adminpage}/>
         </Switch>
       </div>
